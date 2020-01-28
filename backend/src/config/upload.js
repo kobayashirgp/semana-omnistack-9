5b8aa317bc2c1da216e93283a597400d1e1da04d
mateus_mtks11@hotmail.com <mateus_mtks11@hotmail.com>
2020-01-27 23:00:21 -0300

    Adiciona configuração cors

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -1,18 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
 
-const routes = require("./routes");
+module.exports = {
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, "..", "..", "uploads"),
+        filename: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            const name = path.basename(file.originalname, ext);
 
-const app = express();
-
-mongoose.connect(
-    "mongodb+srv://koba:koba1234@koba-k30n9.mongodb.net/aircnc?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
-
-app.use(express.json());
-app.use(routes);
-
-app.listen(8080);
+            cb(null, `${name}-${Date.now()}${ext}`);
+        }
+    })
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routes = require("./routes");
 
 const app = express();
 
 mongoose.connect(
-    "mongodb+srv://koba:koba1234@koba-k30n9.mongodb.net/aircnc?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+  "mongodb+srv://koba:koba1234@koba-k30n9.mongodb.net/aircnc?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
